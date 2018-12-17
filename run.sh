6d18ee1388ba06c4fd261d9aa514841b9416c4dd
BianCai <kpmoving@gmail.com>
2018-12-17 09:17:31 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"; pwd
-python sina_blog_crawler.py http://blog.sina.com.cn/u/2670011074 desc
+python sina_blog_crawler.py http://blog.sina.com.cn/u/2670011074 asc
 
 if [ $(git status|grep docs/Post_|wc -l) -gt 0 ] 
 then 
