3a978ba1cf3cfd12554fbb78a82cf700d66f2631
BianCai <kpmoving@gmail.com>
2018-12-24 10:19:22 +0800

    update

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ python sina_blog_crawler.py http://blog.sina.com.cn/u/2670011074 asc
 if [ $(git status|grep docs/Post_|wc -l) -gt 0 ] 
 then 
 bash docs/genIndex.sh
+date
 git status
 git add *
 git commit  -m "update"
